20f9dcc0454444733472cc12068977d51fc528d9
LLebronC <l.lebron.c@gmail.com>
2017-01-09 21:56:28 +0100

    add plot grafics

--- week2/computed_task_1.m
+++ week2/computed_task_1.m
@@ -11,6 +11,14 @@ function [sequence_db]=computed_task_1(sequence_db,groundtruth_path,groundtruth_
       sequence_db.(fields{i}).Recall=0;
       sequence_db.(fields{i}).F1_score=0;
       
+      sequence_db.(fields{i}).TP_array=[];
+      sequence_db.(fields{i}).TN_array=[];
+      sequence_db.(fields{i}).FP_array=[];
+      sequence_db.(fields{i}).FN_array=[];
+      sequence_db.(fields{i}).Precision_array=[];
+      sequence_db.(fields{i}).Recall_array=[];
+      sequence_db.(fields{i}).F1_score_array=[];
+      
       sequence_part=sequence_db.(fields{i});
       numb_frames=sequence_part.frames(2)-sequence_part.frames(1)+1;
       frames_sequence=sequence_part.frames(1):sequence_part.frames(2);
@@ -21,7 +29,9 @@ function [sequence_db]=computed_task_1(sequence_db,groundtruth_path,groundtruth_
       for count_train=1:train_frames
           
 
-          index_frame=randi(size(frames_sequence,2));
+%           index_frame=randi(size(frames_sequence,2));%to improve the
+%           model because it add random
+          index_frame=count_train;
           train_frame=frames_sequence(index_frame);
           frames_sequence(index_frame)=[];%erase the index from the range
           train_image=imread(strcat(sequence_part.path,input_path,input_name,sprintf('%06d',train_frame),'.jpg'));
@@ -33,23 +43,39 @@ function [sequence_db]=computed_task_1(sequence_db,groundtruth_path,groundtruth_
       %compute the mean and the standart desviation
       mean_value=mean(train_sequence_image,3);
       std_value=std(double(train_sequence_image),0,3);
+      imwrite(uint8(mean_value),strcat(fields{i},'_mean.png'))
+      imwrite(uint8(std_value),strcat(fields{i},'_std.png'))
       
       %test part
-      for count_test=1:size(frames_sequence,2)
-          test_frame=frames_sequence(count_test);
-          test_image=rgb2gray(imread(strcat(sequence_part.path,input_path,input_name,sprintf('%06d',test_frame),'.jpg')));
-          gt_image=imread(strcat(sequence_part.path,groundtruth_path,groundtruth_name,sprintf('%06d',test_frame),'.png'));
-          train_image_res=abs(double(test_image)-mean_value)>=sequence_part.alpha.*(std_value+2);
-          [TP FP FN TN] = compare_images(train_image_res, gt_image);
-          sequence_db.(fields{i}).TP=sequence_db.(fields{i}).TP+TP;
-          sequence_db.(fields{i}).TN=sequence_db.(fields{i}).TN+TN;
-          sequence_db.(fields{i}).FP=sequence_db.(fields{i}).FP+FP;
-          sequence_db.(fields{i}).FN=sequence_db.(fields{i}).FN+FN;
-      end
-      
-       [precision recall F1_score]=get_measures(sequence_db.(fields{i}).TP,sequence_db.(fields{i}).TN,sequence_db.(fields{i}).FP,sequence_db.(fields{i}).FN);
-       sequence_db.(fields{i}).Precision=precision;
-       sequence_db.(fields{i}).Recall=recall;
-       sequence_db.(fields{i}).F1_score=F1_score;
+       for count_alpha =1:size(sequence_part.alpha,2)
+          alpha=sequence_part.alpha(count_alpha);
+          sequence_db.(fields{i}).TP=0;
+          sequence_db.(fields{i}).TN=0;
+          sequence_db.(fields{i}).FP=0;
+          sequence_db.(fields{i}).FN=0;
+          for count_test=1:size(frames_sequence,2)
+              test_frame=frames_sequence(count_test);
+              test_image=rgb2gray(imread(strcat(sequence_part.path,input_path,input_name,sprintf('%06d',test_frame),'.jpg')));
+              gt_image=imread(strcat(sequence_part.path,groundtruth_path,groundtruth_name,sprintf('%06d',test_frame),'.png'));
+              train_image_res=abs(double(test_image)-mean_value)>=alpha.*(std_value+2);
+              [TP FP FN TN] = compare_images(train_image_res, gt_image);
+              sequence_db.(fields{i}).TP=sequence_db.(fields{i}).TP+TP;
+              sequence_db.(fields{i}).TN=sequence_db.(fields{i}).TN+TN;
+              sequence_db.(fields{i}).FP=sequence_db.(fields{i}).FP+FP;
+              sequence_db.(fields{i}).FN=sequence_db.(fields{i}).FN+FN;
+
+
+          end
+            sequence_db.(fields{i}).TP_array=[sequence_db.(fields{i}).TP_array,sequence_db.(fields{i}).TP];
+            sequence_db.(fields{i}).TN_array=[sequence_db.(fields{i}).TN_array,sequence_db.(fields{i}).TN];
+            sequence_db.(fields{i}).FP_array=[sequence_db.(fields{i}).FP_array,sequence_db.(fields{i}).FP];
+            sequence_db.(fields{i}).FN_array=[sequence_db.(fields{i}).FN_array,sequence_db.(fields{i}).FN];
+            [precision recall F1_score]=get_measures(sequence_db.(fields{i}).TP,sequence_db.(fields{i}).TN,sequence_db.(fields{i}).FP,sequence_db.(fields{i}).FN);
+
+           
+           sequence_db.(fields{i}).Precision_array=[sequence_db.(fields{i}).Precision_array,precision];
+           sequence_db.(fields{i}).Recall_array=[sequence_db.(fields{i}).Recall_array,recall];
+           sequence_db.(fields{i}).F1_score_array=[sequence_db.(fields{i}).F1_score_array,F1_score];
+       end
     end
 end
---
+++ week2/plot_grafics.m
@@ -0,0 +1,47 @@
+function plot_grafics(sequence_db)
+    fields = fieldnames(sequence_db);
+    colors=['b','g','r'];
+    figure()
+    title('F1-score vs alpha');
+    hold on;
+    xlabel('alphas')
+    ylabel('F1 score')
+    for i=1:numel(fields)
+
+        plot(sequence_db.(fields{i}).alpha,sequence_db.(fields{i}).F1_score_array,colors(i))
+    end
+    legend(fields)
+    hold off;
+    saveas(gcf, 'F1-score_vs_alpha', 'png')
+    
+    figure()
+    title('Precision vs Recall');
+    hold on;
+    xlabel('Recall')
+    ylabel('Precision')
+    legend_info=cell(numel(fields,1));
+    for i=1:numel(fields)
+
+        plot(sequence_db.(fields{i}).Recall_array,sequence_db.(fields{i}).Precision_array,colors(i))
+        auc_class = trapz(sequence_db.(fields{i}).Recall_array,sequence_db.(fields{i}).Precision_array);
+        legend_info{i}=strcat(fields{i},' AUC: ',sprintf('%.2f',abs(auc_class)));
+    end
+    legend(legend_info)
+    hold off;
+    saveas(gcf, 'Precision_vs_Recall', 'png')
+    
+    for i=1:numel(fields)
+        figure()
+        title(strcat(fields{i},' TP,FP,FP,FN'));
+        hold on;
+        plot(sequence_db.(fields{i}).alpha,sequence_db.(fields{i}).TP_array)
+        plot(sequence_db.(fields{i}).alpha,sequence_db.(fields{i}).TN_array)
+        plot(sequence_db.(fields{i}).alpha,sequence_db.(fields{i}).FP_array)
+        plot(sequence_db.(fields{i}).alpha,sequence_db.(fields{i}).FN_array)
+        
+        legend('TP','TN','FP','FN')
+        hold off;
+        saveas(gcf, strcat(fields{i},'_metric'), 'png')
+    end
+
+end
