2b04045c95634eb1f7931381bdcfc4c221cffc65
JoseLGomez <joseluis-master@hotmail.com>
2017-01-09 15:11:45 +0100

    Add files via upload

--- week2/task2.m
+++ week2/task2.m
@@ -11,11 +11,11 @@ datasets = dir('Dataset/');
 datasets = datasets(find(vertcat(datasets.isdir)));
 datasets = datasets(3:end);
 
-%------------------
-%Gaussian Modelling
-%------------------
-
-
+%-------Task 4-----------%
+alphaTest = {'Fall' 'Highway' 'Traffic'};
+alphaParam = [2, 1.5, 1.7];
+alphaMap = containers.Map(alphaTest,alphaParam);
+TP = zeros([length(datasets) (param.maxP+abs(param.minP))/0.1]); FP=TP;FN=FP;TN=FN;F1=TN;Recall=F1;Precision=Recall;
 for i=1:length(datasets)
     
     %Training 
@@ -30,103 +30,43 @@ for i=1:length(datasets)
     
     [mean_dataset{i}, sd_dataset{i}] = GaussianTraining(inputFolder,dirList,numberTraining,param.gaussianColor);
     
-    %Testing
-    %-------
-    %[output{i}] = GaussianTesting(inputFolder,dirList,mean_dataset{i},sd_dataset{i},numberTraining,param.gaussianColor,param.alpha,true);
-
-end
-
-%-------Task 2/3-----------
-TP = zeros([length(datasets) (param.maxAlpha+abs(param.minAlpha))/0.1]); FP=TP;FN=FP;TN=FN;F1=TN;Recall=F1;Precision=Recall;
-for i=1:length(datasets)
-    %List the testing images
-    inputFolder = strcat('Dataset/',datasets(i).name,'/input/');
-    dirList = dir(inputFolder);
-    dirList = dirList(3:end);
-    %List the groundtruth images
-    groundtruthFolder = strcat('Dataset/',datasets(i).name,'/groundtruth/')
+%%   
+   %Groundtruth
+   %List the groundtruth images
+    groundtruthFolder = strcat('Dataset/',datasets(i).name,'/groundtruth/');
     groundtruthList = dir(groundtruthFolder);
     groundtruthList = groundtruthList(3:end);
-    numberTraining = floor(length(dirList) / 2);
-    
-    t=1;
-    
-    %Inicialize the matrix
     imsize = size(imread(strcat(groundtruthFolder,groundtruthList(1).name)));
     groundtruth = zeros([imsize(1:2) length(dirList)-numberTraining+1 ]);
-    
     for ii=numberTraining+1:length(dirList)
-        if(param.gaussianColor)
-            groundtruth(:,:,:,ii-numberTraining) = imread(strcat(groundtruthFolder,strrep(groundtruthList(ii).name,'in','gt')));
-        else
-            groundtruth(:,:,ii-numberTraining) = imread(strcat(groundtruthFolder,strrep(groundtruthList(ii).name,'in','gt')));
-        end
-    end;
-    
-    for ii=param.minAlpha:0.1:param.maxAlpha
-        [output] = GaussianTesting(inputFolder,dirList,mean_dataset{i},sd_dataset{i},numberTraining,param.gaussianColor,ii,false,datasets(i).name);
-        [TP(i,t),FP(i,t),FN(i,t),TN(i,t),F1(i,t),Recall(i,t),Precision(i,t)] = GaussianEvaluation(output,groundtruth);
-        t=t+1;
+           groundtruth(:,:,ii-numberTraining) = imread(strcat(groundtruthFolder,strrep(groundtruthList(ii).name,'in','gt')));
     end
-end
-
-%Ploting Data
-for i=1:length(datasets)
-    len = length(TP(i,:));
-    figure,
-    plot((1:len),TP(i,:),(1:len), FP(i,:),(1:len), FN(i,:),(1:len), TN(i,:))
-%     set(gca,'XTick',1:10:len )
-     set(gca,'XTickLabel',param.minAlpha:0.1:param.maxAlpha )
-    legend('TP','FP','FN','TN')
-    title(strcat('Evaluation - Non-recursive Gaussian modeling - "',datasets(i).name,'" Dataset'))
-    xlabel('\alpha Threshold')
-    ylabel('n. of Pixels')
-end
-
-for i=1:length(datasets)
-    figure,
-    plot(1:length(F1(i,:)),F1(i,:))
-    %Max F1
-    [y x] = max(F1(i,:))
-    hold on
-    plot(x,y,'o','MarkerSize',10)
-    strmax = ['\alpha = ',num2str(x*0.1), ' | F1 Score = ',num2str(F1(i,x))];
-    text(x,y,strmax,'HorizontalAlignment','left');
-    %Min F1
-    [y x] = min(F1(i,:))
-    hold on
-    plot(x,y,'o','MarkerSize',10)
-    strmax = ['\alpha = ',num2str(x*0.1), ' | F1 Score = ',num2str(F1(i,x))];
-    text(x,y,strmax,'HorizontalAlignment','left');
-    %Plot parameters
-%     set(gca,'XTick',1:10:len )
-    set(gca,'XTickLabel',param.minAlpha:0.1*10:param.maxAlpha )
-    title(strcat('F1 Score - Non-recursive Gaussian modeling - "',datasets(i).name,'" Dataset'))
-    xlabel('\alpha Threshold')
-    ylabel('F1 Score')
-end
-
-%%precision vs Recall curve & auc
-for i=1:length(datasets)
- figure()
- plot(Precision(i,:),Recall(i,:))
- title(strcat('Precision vs Recall - Recursive Gaussian modeling - "',datasets(i).name,'" Dataset',' AUC=',num2str(trapz(Recall(i,:),Precision(i,:)))))
- xlabel('Recall')
- ylabel('Precision')
-end
-
+   % Classify
+   t=1;
+   for p=param.minP:0.1:param.maxP 
+       i_img=1;
+       background = zeros([imsize(1:2) length(dirList)-numberTraining+1 ]);
+        for j = numberTraining+1:length(dirList)
+            [mean_dataset{i}, sd_dataset{i},background(:,:,i_img)] = GaussianAdaptativeClassify (inputFolder,dirList,j,param.gaussianColor,sd_dataset{i},mean_dataset{i},alphaParam(i),p,true,datasets(i).name);
+            i_img=i_img+1;
+        end
+        %Evaluation
+       [TP(i,t),FP(i,t),FN(i,t),TN(i,t),F1(i,t),Recall(i,t),Precision(i,t)] = GaussianEvaluation(background,groundtruth);
+        t=t+1;
+   end
+end   
 %% Results
 % TP, FP, FN, TN
-% for i=1:length(datasets)
-%     len = length(TP(i,:));
-%     figure()
-%     plot((1:len),TP(i,:),(1:len), FP(i,:),(1:len), FN(i,:),(1:len), TN(i,:))
-%     set(gca,'XTickLabel',param.minP:0.1:param.maxP )
-%     legend('TP','FP','FN','TN')
-%     title(strcat('Evaluation - Recursive Gaussian modeling - "',datasets(i).name,'" Dataset'))
-%     xlabel('p Threshold')
-%     ylabel('Nï¿½ Pixels')
-% end
+for i=1:length(datasets)
+    len = length(TP(i,:));
+    figure()
+    plot((1:len),TP(i,:),(1:len), FP(i,:),(1:len), FN(i,:),(1:len), TN(i,:))
+    set(gca,'XTickLabel',param.minP:0.1:param.maxP )
+    legend('TP','FP','FN','TN')
+    title(strcat('Evaluation - Recursive Gaussian modeling - "',datasets(i).name,'" Dataset'))
+    xlabel('p Threshold')
+    ylabel('Nº Pixels')
+end
 %%
 %F1 score
 for i=1:length(datasets)
@@ -136,13 +76,13 @@ for i=1:length(datasets)
     [y x] = max(F1(i,:))
     hold on
     plot(x,y,'o','MarkerSize',10)
-    strmax = ['p = ',num2str(x*0.1), ' | F1 Score = ',num2str(F1(i,x))];
+    strmax = ['p = ',num2str((x-1)*0.1), ' | F1 Score = ',num2str(F1(i,x))];
     text(x,y,strmax,'HorizontalAlignment','left');
     %Min F1
     [y x] = min(F1(i,:))
     hold on
     plot(x,y,'o','MarkerSize',10)
-    strmax = ['p = ',num2str(x*0.1), ' | F1 Score = ',num2str(F1(i,x))];
+    strmax = ['p = ',num2str((x-1)*0.1), ' | F1 Score = ',num2str(F1(i,x))];
     text(x,y,strmax,'HorizontalAlignment','left');
     %Plot parameters
 %     set(gca,'XTick',1:10:len )
