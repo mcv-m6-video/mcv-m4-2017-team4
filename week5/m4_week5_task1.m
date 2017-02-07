306e274edf7ad5ddc4ebee248bb88da723bf97d0
LLebronC <l.lebron.c@gmail.com>
2017-02-08 00:08:35 +0100

    add count car and speed mean

--- week5/m4_week5_task1.m
+++ week5/m4_week5_task1.m
@@ -10,24 +10,24 @@ function m4_week5_task1()
     sequence_db=struct;
     
     
-%     sequence_db.frames=[950 1050];
-%     sequence_db.path='./traffic/';
-%     params.reference=[20 83; 136 1; 135 240;320 111];
-%     sequence=[sequence_db.frames(1):sequence_db.frames(2)];
-%     params.time=3.0/30.0;
-%     params.km=3.6;
-%     params.pixel=9.0/78.0;
-    
-    
-    
-    
-    sequence_db.frames=[1050, 1350];
-    sequence_db.path='./highway/';
-    params.reference=[10.0522 190.8483;190.7488 24.0821;273.9328 20.1020;263.9826 239.4055];
+    sequence_db.frames=[950 1050];
+    sequence_db.path='./traffic/';
+    params.reference=[20 83; 136 1; 135 240;320 111];
     params.fps=25;
     params.km=3.6;
-    params.pixel=10/13;
+    params.pixel=4.5/125;
     sequence=[sequence_db.frames(1):sequence_db.frames(2)];
+    
+    
+    
+    
+%     sequence_db.frames=[1050, 1350];
+%     sequence_db.path='./highway/';
+%     params.reference=[10.0522 190.8483;190.7488 24.0821;273.9328 20.1020;263.9826 239.4055];
+%     params.fps=25;
+%     params.km=3.6;
+%     params.pixel=10/13;
+%     sequence=[sequence_db.frames(1):sequence_db.frames(2)];
 
     
     
@@ -38,13 +38,6 @@ function m4_week5_task1()
     
     input_path='input/';
     input_name='in';
-%     new_folder='./traffic/input/';
-%     new_gt='./traffic/groundtruth/';
-    
-
-    
-    
-
     
     [params.mean,params.var]=compute_mean_std(sequence_db,input_path,input_name);
     
@@ -53,7 +46,9 @@ function m4_week5_task1()
     obj = setupSystemObjects();
     tracks = initializeTracks(); % Create an empty array of tracks.
 
+    speed_meter=cell(1,1);
     nextId = 1; % ID of the next track
+    real_count_tracks=1;
     trackedObjs = [];
     H=homografia(params);
     for count_seq=1:size(sequence,2)
@@ -80,7 +75,16 @@ function m4_week5_task1()
 
         displayTrackingResults();
     end
-
+    disp('num_cars')
+    disp(real_count_tracks-1)
+    disp('speed')
+    for count_speed=1:real_count_tracks-1
+        disp('car')
+        disp(count_speed)
+        disp('mean_speed')
+        disp(mean(speed_meter{count_speed}))
+    end
+    
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     function obj = setupSystemObjects()
         % Initialize Video I/O
@@ -115,6 +119,7 @@ function m4_week5_task1()
         % create an empty array of tracks
         tracks = struct(...
             'id', {}, ...
+            'real_id', 0, ...
             'bbox', {}, ...
             'kalmanFilter', {}, ...
             'age', {}, ...
@@ -248,6 +253,7 @@ function m4_week5_task1()
             % Create a new track.
             newTrack = struct(...
                 'id', nextId, ...
+                'real_id', 0, ...
                 'bbox', bbox, ...
                 'kalmanFilter', kalmanFilter, ...
                 'age', 1, ...
@@ -276,12 +282,18 @@ function m4_week5_task1()
 
         minVisibleCount = 8;
         if ~isempty(tracks)
-
+            %update the new cars
+            for count_i=1:length(tracks)
+                if tracks(count_i).totalVisibleCount> minVisibleCount && tracks(count_i).real_id ==0
+                    tracks(count_i).real_id=real_count_tracks;
+                    speed_meter{tracks(count_i).real_id}=[];
+                    real_count_tracks=real_count_tracks+1;
+                end
+            end
             % Noisy detections tend to result in short-lived tracks.
             % Only display tracks that have been visible for more than
             % a minimum number of frames.
-            reliableTrackInds = ...
-                [tracks(:).totalVisibleCount] > minVisibleCount;
+            reliableTrackInds = [tracks(:).totalVisibleCount] > minVisibleCount;
             reliableTracks = tracks(reliableTrackInds);
 
             
@@ -292,6 +304,7 @@ function m4_week5_task1()
                 if size(trackHistorial.centroid, 1) > 1
                     speed = compute_speed(trackHistorial, H,params);
                     trackedObjs{reliableTracks(i).id}.speed = speed;
+                    speed_meter{reliableTracks(i).real_id}=[speed_meter{reliableTracks(i).real_id},speed];
                     speeds{i} = ['  ' num2str(speed)];
                 else
                     speeds{i} = ['  ' num2str(0)];
@@ -306,7 +319,7 @@ function m4_week5_task1()
                 bboxes = cat(1, reliableTracks.bbox);
 
                 % Get ids.
-                ids = int32([reliableTracks(:).id]);
+                ids = int32([reliableTracks(:).real_id]);
 
                 % Create labels for objects indicating the ones for
                 % which we display the predicted rather than the actual
